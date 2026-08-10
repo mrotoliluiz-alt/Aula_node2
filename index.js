ce81b06b182160a3f9cbb16c5b354a19b5e660d4
mrotoliluiz-alt <mrotoliluiz@gmail.com>
2026-08-10 08:08:32 -0300

    o bagui feito sla

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const porta = process.env.PORT || 3333;
+
+app.listen(porta, () => {
+    console.log(`Servidor iniciado em http://localhost:${porta}`);
+});
